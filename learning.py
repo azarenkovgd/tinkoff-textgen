18db83741950def596c8e6b2c2c8d27159060f4f
azarenkovgd <you@example.com>
2020-09-20 18:58:50 +0300

    small changes

--- learning.py
+++ learning.py
@@ -7,13 +7,17 @@ if __name__ == '__main__':
     parser.add_argument('input_path', type=str, help=('Расположение файла для обучения в формате .txt '
                                                       'или расположение папки с файлами для обучения в '
                                                       'формате .txt'))
-    parser.add_argument('--folder_to_save_model', '-f', type=str, default='models', help='Папка для сохранения модели')
+    parser.add_argument('--folder_to_save_model', '-f', type=str, default='', help='Папка для сохранения модели')
     parser.add_argument('--name_to_save_model', '-n', type=str, default='model', help='Имя модели для сохранения')
     args = parser.parse_args()
 
-    tokenizer.check_path(args.folder_to_save_model)
+    if args.folder_to_save_model != '':
+        tokenizer.check_path(args.folder_to_save_model)
+        path_to_save_model = f'{args.folder_to_save_model}/{args.name_to_save_model}'
+    else:
+        path_to_save_model = args.name_to_save_model
+
     tokens = tokenizer.get_tokens(args.input_path)
-    path_to_save_model = f'{args.folder_to_save_model}/{args.name_to_save_model}'
 
     print('Обучение начато.')
     gen_model = model.TextGenModel()
