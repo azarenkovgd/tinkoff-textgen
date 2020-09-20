1f0c2c3b25e1dafbc19a5c902ef0b008e6590a95
azarenkovgd <you@example.com>
2020-09-20 19:08:07 +0300

    change default folder for saving model

--- learning.py
+++ learning.py
@@ -7,20 +7,14 @@ if __name__ == '__main__':
     parser.add_argument('input_path', type=str, help=('Расположение файла для обучения в формате .txt '
                                                       'или расположение папки с файлами для обучения в '
                                                       'формате .txt'))
-    parser.add_argument('--folder_to_save_model', '-f', type=str, default='', help='Папка для сохранения модели')
-    parser.add_argument('--name_to_save_model', '-n', type=str, default='model', help='Имя модели для сохранения')
+    parser.add_argument('--path_to_save_model', '-s', type=str, default='textgen_model', help='Путь для сохранения '
+                                                                                              'модели')
     args = parser.parse_args()
-
-    if args.folder_to_save_model != '':
-        tokenizer.check_path(args.folder_to_save_model)
-        path_to_save_model = f'{args.folder_to_save_model}/{args.name_to_save_model}'
-    else:
-        path_to_save_model = args.name_to_save_model
 
     tokens = tokenizer.get_tokens(args.input_path)
 
     print('Обучение начато.')
     gen_model = model.TextGenModel()
     gen_model.fit(tokens)
-    gen_model.save(path_to_save_model)
-    print(f'Обучение прошло успешно. Модель сохранена в {path_to_save_model}')
+    gen_model.save(args.path_to_save_model)
+    print(f'Обучение прошло успешно. Модель сохранена в {args.path_to_save_model}')
